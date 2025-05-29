205dbf1283f177ea1ad6f773b17be01cc74a200f
akshayeanil <akshayanil770@gmail.com>
2025-05-28 21:03:18 -0400

    Update test_weather.py

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,12 +1,14 @@
 import unittest
+
 from weather import get_weather
 
+
 class TestWeather(unittest.TestCase):
+
     def test_get_weather(self):
         # This is a simple test placeholder
         self.assertIsNone(get_weather("London"))
 
+
 if __name__ == "__main__":
     unittest.main()
-
-
